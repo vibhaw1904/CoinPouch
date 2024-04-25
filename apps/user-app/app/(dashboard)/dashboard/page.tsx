21e7bcbef9e530ec3556e35e811b70c35bde6ef8
vibhaw raj <vibhawraj494@gmail.com>
2024-04-26 01:52:01 +0530

    [feat]:added graph for transactions

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { authOptions } from "../../lib/auth"
 import prisma from "@repo/db/client"
 import { User } from "../../../components/User";
 import { Card } from "@repo/ui/card";
+import { UserGraph } from "../../../components/UserGraph";
+import { timeStamp } from "console";
 async function getAllUsers() {
     try {
       const users = await prisma.user.findMany();
@@ -14,21 +16,49 @@ async function getAllUsers() {
       await prisma.$disconnect();
     }
   }
+export async function getTransaction(){
+  const session=await getServerSession(authOptions)
+  const transactions=await prisma.p2pTransfer.findMany({
+      where:{
+        OR:[
+          {fromUserId:Number(session?.user?.id)},
+          {toUserId:Number(session?.user?.id)}
+        ]
 
+      }
+  })
+  return transactions.map((u)=>({
+    amount:u.amount,
+    timestamp:u.timestamp,
+    fromUserId:u.fromUserId,
+    toUserId:u.toUserId,
+    currentUser:Number(session?.user?.id)
+  }));
+}
 export default async function() {
     const users=await getAllUsers()
     const session = await getServerSession(authOptions);
+    const traansaction=await getTransaction()
     console.log(session)
     const loggedInUserId = session?.user?.id; 
     console.log(loggedInUserId)
     const otherUsers = users.filter((user) => user.id != loggedInUserId);
     console.log(otherUsers)
-    return <Card title="users">
-    <div className='my-2'><input className='w-full px-2 py-1 border rounded border-slate-200' placeholder='Search user' /></div>
-      <div >
-      {otherUsers.map((user) => (
-          <User key={user.id} user={user} />
-        ))}
-      </div>
-    </Card>
+    return   <div className=" m-[3%]">
+    <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        Dashboard
+    </div>
+    <UserGraph transaction={traansaction.reverse()}></UserGraph>
+    <div className="flex justify-center items-center w-full">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-8 p-4">
+            <div className="lg:col-span-3">
+                <User user={otherUsers} ></User>
+            </div>
+            {/* <div className="lg:col-span-5">
+                <P2PTransaction transactions={trsx}></P2PTransaction>
+            </div> */}
+        </div>
+    </div>
+</div>
+
 }
--- apps/user-app/components/User.tsx
+++ apps/user-app/components/User.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { Button } from '@repo/ui/button';
+import { Card } from '@repo/ui/card';
 import React from 'react'; 
 
 export const User = ({
   user,
-  key
+  
 }: {
   user: {
     id: number;
@@ -12,8 +13,8 @@ export const User = ({
     name: string | null;
     number: string;
     password: string;
-  };
-  key:number
+  }[];
+  
 }) => {
 
   const handleSendMoneyClick = () => {
@@ -23,23 +24,28 @@ export const User = ({
   };
 
   return (
+    <Card title="Available users">
+  {user.map((u)=>(
     <div className='flex flex-row justify-between mt-4 w-full'>
-      <div className='flex justify-between'>
-        <div className='rounded-full w-12 h-12 flex justify-center mt-2 mr-2 bg-slate-200'>
-          <div className='flex flex-col justify-center h-full text-xl'>
-          v
-          </div>
-        </div>
-        <div className='flex flex-col h-full justify-center'key={user.id}>
-          <div>{user.name}</div>
+    <div className='flex justify-between'>
+      <div className='rounded-full w-12 h-12 flex justify-center mt-2 mr-2 bg-slate-200'>
+        <div className='flex flex-col justify-center h-full text-xl'>
+        v
         </div>
       </div>
-  
-      <div className='flex flex-row h-full justify-between'>
-      {user.number}
-
-      <Button onClick={handleSendMoneyClick} >Send money</Button>
+      <div className='flex flex-col h-full justify-center'key={u.id}>
+        <div>{u.name}</div>
       </div>
     </div>
+
+    <div className='flex flex-row h-full justify-between'>
+    {u.number}
+
+    <Button onClick={handleSendMoneyClick} >Send money</Button>
+    </div>
+  </div>
+  ))} 
+    </Card>
+   
   );
 };
